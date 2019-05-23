4780df1faa0aded4a696f425e5cdae5f636809e9
Luis H. Porras <luishercep@gmail.com>
2019-05-23 04:32:56 +0200

    Create basic components

--- src/front/src/components/Reservoir.vue
+++ src/front/src/components/Reservoir.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <svg class="editorial"
+  <div class="reservoir" style="height: 800px">
+    <svg
       xmlns="http://www.w3.org/2000/svg"
       xmlns:xlink="http://www.w3.org/1999/xlink"
       viewBox="0 24 150 28"
@@ -20,17 +20,17 @@
         <use xlink:href="#gentle-wave" x="50" y="6" fill="#2d55aa"/>
       </g>
     </svg>
-    <div class="content">
-      <h1 contenteditable="true">The deep blue</h1>
+    <div class="title">
+      <h1 contenteditable="true">Embalsamando "The deep blue"</h1>
     </div>
 
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet">
-  <div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Reservoir',
   props: {
     msg: String
   }
@@ -39,18 +39,85 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+::selection {
+	background-color: salmon;
+	color: white;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.parallax > use {
+	animation: move-forever 12s linear infinite;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.parallax > use:nth-child(1) {
+	animation-delay: -2s;
 }
-a {
-  color: #42b983;
+
+.parallax > use:nth-child(2) {
+	animation-delay: -2s;
+	animation-duration: 5s;
+}
+
+.parallax > use:nth-child(3) {
+	animation-delay: -4s;
+	animation-duration: 3s;
+}
+
+@keyframes move-forever {
+	0% {
+		transform: translate(-90px, 0%);
+	}
+
+	100% {
+		transform: translate(85px, 0%);
+	}
+}
+
+.reservoir svg {
+	display: block;
+	width: 100%;
+	height: 10em;
+	max-height: 100vh;
+	margin: 0;
+}
+
+.title {
+	font-family: 'Lato',sans-serif;
+	text-align: center;
+	background-color: #2d55aa;
+	text-align: center;
+	min-height: 75vh;
+	margin: -.1em 0 0 0;
+	padding: 1em;
+	color: #eee;
+	font-size: 2em;
+	font-weight: 300;
+	font-size: 1em;
+}
+
+.title h1 {
+	margin-top: -1.3em;
+	font-weight: 100;
+	font-size: 5em;
+	user-select: none;
+	cursor: default;
+}
+
+/*prevent many large-by-comparison ripples by shrinking the height*/
+@media (max-width: 50em) {
+	.title h1 {
+		font-size: 12vmax;
+	}
+
+	.editorial {
+		height: 17vw;
+	}
+}
+
+h1 {
+	outline: none;
+}
+
+body {
+	background-color: #234;
 }
 </style>
---
+++ src/front/src/components/Select.vue
@@ -0,0 +1,61 @@
+<template>
+  <div style="height: 200px">
+		<h1>
+			Latitude is: <b-badge>{{ coords.lat}}</b-badge><br>
+			Longitude is: <b-badge>{{ coords.long}}</b-badge>
+		</h1>
+		<p v-if="isGeoAvailable === false">
+			Geolocation not available
+		</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Select',
+  data () {
+    return {
+			info: null,
+			isGeoAvailable: true,
+      error: null,
+      coords: {
+        lat: '',
+        long: ''
+      }
+    }
+  },
+  mounted () {
+		this.getLocation()
+		// axios
+    //   .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+		//   .then(response => (this.info = response))
+	},
+	methods: {
+		getLocation () {
+				if ('geolocation' in navigator) {
+					navigator.geolocation
+						.getCurrentPosition((position) => {
+							const lat = position.coords.latitude
+							const long = position.coords.longitude
+							this.coords.lat = lat
+							this.coords.long = long
+				}, (error) => {
+            this.error = {
+              code: error.code,
+              msg: error.message
+            }
+            console.error(error)
+          })
+      } else {
+        this.isGeoAvailable = false
+      }
+		}
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
